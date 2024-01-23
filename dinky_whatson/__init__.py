968a2a4f5c686980748332ba01315caa63ebbac3
Julian Prester <hi@julianprester.com>
2024-01-23 23:38:56 +1100

    Package fonts

--- dinky_whatson/__init__.py
+++ dinky_whatson/__init__.py
@@ -5,6 +5,8 @@ import json
 import textwrap
 from datetime import date
 from PIL import Image, ImageFont, ImageDraw
+import pkg_resources
+from io import BytesIO
 
 from dinky.layouts.layout_configuration import Zone
 
@@ -33,14 +35,21 @@ class DinkyWhatsOnPlugin:
         events = self._get_events()
         im = Image.new('RGB', (zone.width, zone.height), (255, 255, 255))
         draw = ImageDraw.Draw(im)
-        fnt_header = ImageFont.truetype("arial.ttf", 36)
-        fnt_title = ImageFont.truetype("arialbd.ttf", 14)
-        fnt_regular = ImageFont.truetype("arial.ttf", 14)
+
+        font_data = pkg_resources.resource_stream('dinky_whatson', 'fonts/Roboto-Regular.ttf')
+        font_bytes = BytesIO(font_data.read())
+        font_header = ImageFont.truetype(font_bytes, 36)
+        font_bytes.seek(0)
+        font_regular = ImageFont.truetype(font_bytes, 14)
+        font_data = pkg_resources.resource_stream('dinky_whatson', 'fonts/Roboto-Bold.ttf')
+        font_bytes = BytesIO(font_data.read())
+        font_title = ImageFont.truetype(font_bytes, 14)
+
         draw.rectangle((zone.padding, zone.padding, zone.width-zone.padding, zone.padding + 55), fill=self.primary_color)
-        draw.text((zone.padding + 5, zone.padding + 5), f"What's On", font=fnt_header, fill="white")
+        draw.text((zone.padding + 5, zone.padding + 5), f"What's On", font=font_header, fill="white")
         for i, event in enumerate(events):
             wrapper = textwrap.TextWrapper(width=int(0.15 * (zone.width - 2 * zone.padding)), max_lines=1)
-            draw.text((zone.padding + 5, 55 + zone.padding + 5 + (55 * i)), wrapper.fill(event["name"]), font=fnt_title, fill=self.primary_color)
+            draw.text((zone.padding + 5, 55 + zone.padding + 5 + (55 * i)), wrapper.fill(event["name"]), font=font_title, fill=self.primary_color)
             wrapper = textwrap.TextWrapper(width=int(0.15 * (zone.width - 2 * zone.padding)), max_lines=2)
-            draw.multiline_text((zone.padding + 5, 55 + zone.padding + 5 + (55 * i) + 18), wrapper.fill(event["strapline"]), font=fnt_regular, fill="black")
+            draw.multiline_text((zone.padding + 5, 55 + zone.padding + 5 + (55 * i) + 18), wrapper.fill(event["strapline"]), font=font_regular, fill="black")
         return im
